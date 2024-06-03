ebd7a9b4d67aefa5ce9a8ca2a1bbe2512f5947d6
shamim <shamimmahmud077@gmail.com>
2024-06-03 18:25:56 +0600

    get all session card

--- index.js
+++ index.js
@@ -122,6 +122,12 @@ async function run() {
       res.send(result);
     });
 
+    // get all session
+    app.get("/session", async (req, res) => {
+      const result = await sessionCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
